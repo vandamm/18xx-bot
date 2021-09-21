0d31ad19594d543fe1013298b7a38a256ba3042c
Alex Vansach <alex@vansach.me>
2021-09-21 23:56:16 +0300

    feat: implement basic logic for bot

--- src/handlers/send_notifications.ts
+++ src/handlers/send_notifications.ts
@@ -1,7 +1,19 @@
+import { Bot, Incoming18xxMessage } from '../lib/bot.js';
 import { APIGatewayProxyHandler } from 'aws-lambda';
+
+const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   console.log('Event', event);
 
-  return { statusCode: 200, body: 'OK' };
+  try {
+    await bot.notifyUser(JSON.parse(event.body) as Incoming18xxMessage);
+
+    return { statusCode: 200, body: 'OK' };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e.message,
+    };
+  }
 };
