4ce0dfceb40950e8588ba6a7b1d24f53ebdedb69
Alex Vansach <alex@vansach.me>
2021-10-18 15:35:07 +0300

    test: add controller unit tests

--- src/notifications/notifications.controller.spec.ts
+++ src/notifications/notifications.controller.spec.ts
@@ -1,14 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { NotificationsController } from './notifications.controller';
 import { BotService } from '../bot.service';
+
+jest.mock('../common/templates', () => {
+  return {
+    __esModule: true,
+    notificationMessage: (text, link) => `${text}/${link}`,
+  };
+});
+
+jest.mock('../common/18xx_message', () => {
+  return {
+    __esModule: true,
+    Parsed18xxMessage: class {
+      toString() {
+        return 'message';
+      }
+      get link() {
+        return 'link';
+      }
+    },
+  };
+});
+
+import { NotificationsController } from './notifications.controller';
+import { Parsed18xxMessage } from '../common/18xx_message';
 
 describe('NotificationsController', () => {
   let controller: NotificationsController;
+  let sendMessageMock;
 
   beforeEach(async () => {
+    sendMessageMock = jest.fn();
+
+    const BotServiceProvider = {
+      provide: BotService,
+      useValue: {
+        sendMessage: sendMessageMock,
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificationsController],
-      providers: [BotService],
+      providers: [BotServiceProvider],
     }).compile();
 
     controller = module.get<NotificationsController>(NotificationsController);
@@ -16,5 +48,15 @@ describe('NotificationsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call sendMessages', async () => {
+    const chatId = 42;
+    const message = new Parsed18xxMessage(null);
+
+    await controller.sendNotification(chatId, message);
+
+    expect(sendMessageMock.mock.calls.length).toBe(1);
+    expect(sendMessageMock.mock.calls[0]).toEqual([chatId, 'message/link']);
   });
 });
--- src/telegram/telegram.controller.spec.ts
+++ src/telegram/telegram.controller.spec.ts
@@ -1,14 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TelegramController } from './telegram.controller';
 import { BotService } from '../bot.service';
+import { Update } from 'typegram';
 
 describe('TelegramController', () => {
   let controller: TelegramController;
+  let processUpdateMock;
 
   beforeEach(async () => {
+    processUpdateMock = jest.fn();
+
+    const BotServiceProvider = {
+      provide: BotService,
+      useValue: {
+        processUpdate: processUpdateMock,
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TelegramController],
-      providers: [BotService],
+      providers: [BotServiceProvider],
     }).compile();
 
     controller = module.get<TelegramController>(TelegramController);
@@ -16,5 +27,14 @@ describe('TelegramController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should process updates using bot service', async () => {
+    const update = <Update>{};
+
+    await controller.processUpdate(update);
+
+    expect(processUpdateMock.mock.calls.length).toBe(1);
+    expect(processUpdateMock.mock.calls[0]).toEqual([update]);
   });
 });
