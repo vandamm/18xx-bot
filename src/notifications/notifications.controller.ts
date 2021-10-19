dc37c61db8ecac4f71196daec051e23ffe5b0951
Alex Vansach <alex@vansach.me>
2021-10-19 17:30:23 +0300

    feat: log incoming requests

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Parsed18xxMessage } from '../common/18xx_message';
 import { notificationMessage } from '../common/templates';
 import { Parse18xxMessagePipe } from '../common/parse_18xx_message.pipe';
 import { BotService } from '../bot.service';
+import { RequestLoggingInterceptor } from '../common/request_logging_interceptor';
 
 @Controller('send-notifications')
+@UseInterceptors(RequestLoggingInterceptor)
 export class NotificationsController {
   constructor(private readonly botService: BotService) {}
 
