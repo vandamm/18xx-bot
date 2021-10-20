1e16f0f144e59d80e76ad761fe64ee094a46240a
Alex Vansach <alex@vansach.me>
2021-10-20 13:51:36 +0300

    fix: 18ZOO event handling

--- src/common/18xx_message.spec.ts
+++ src/common/18xx_message.spec.ts
@@ -1,7 +1,8 @@
 import { Parsed18xxMessage } from './18xx_message';
 
-const event =
-  '{"text":"<@123456789> Your Turn in 1836Jr30 \\"Test game\\" (Auction Round 1)\\nhttp://18xx.games/game/1234"}';
+const event = {
+  text: '<@123456789> Your Turn in 1836Jr30 "Test game" (Auction Round 1)\nhttp://18xx.games/game/1234',
+};
 
 const emptyMessage = {
   userId: undefined,
@@ -27,19 +28,13 @@ describe('18xx message', () => {
   });
 
   it('does nothing if input is empty', () => {
-    const message = new Parsed18xxMessage(JSON.parse('""'));
-
-    expect(message).toEqual(emptyMessage);
-  });
-
-  it('does nothing if input has no property', () => {
-    const message = new Parsed18xxMessage(JSON.parse('{}'));
+    const message = new Parsed18xxMessage(null);
 
     expect(message).toEqual(emptyMessage);
   });
 
   it('parses message', () => {
-    const message = new Parsed18xxMessage(JSON.parse(event));
+    const message = new Parsed18xxMessage(event);
 
     expect(message).toEqual({
       userId: '123456789',
@@ -53,8 +48,16 @@ describe('18xx message', () => {
     });
   });
 
+  it('parses 18ZOO message', () => {
+    const message = new Parsed18xxMessage({
+      text: '<@Hello> Your Turn in 18ZOO - Map A "test zoo" (SR-day 1)\nhttp://18xx.games/game/59585',
+    });
+
+    expect(message.valid).toBeTruthy();
+  });
+
   it('formats message text back to original', () => {
-    const message = new Parsed18xxMessage(JSON.parse(event));
+    const message = new Parsed18xxMessage(event);
 
     expect(message.toString()).toBe(
       'Your Turn in 1836Jr30 "Test game" (Auction Round 1)',
