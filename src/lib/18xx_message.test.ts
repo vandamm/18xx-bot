b2ee87814da7791ae74aa069abf0104f58601d0c
Alex Vansach <alex@vansach.me>
2022-01-26 17:27:01 +0300

    fix: correctly parse game name with spaces

--- src/lib/18xx_message.test.ts
+++ src/lib/18xx_message.test.ts
@@ -1,4 +1,4 @@
-import { Parsed18xxMessage } from './18xx_message';
+import { Parsed18xxMessage } from "./18xx_message";
 
 const event =
   '{"text":"<@123456789> Your Turn in 1836Jr30 \\"Test game\\" (Auction Round 1)\\nhttp://18xx.games/game/1234"}';
@@ -14,46 +14,54 @@ const emptyMessage = {
   valid: false,
 };
 
-test('does nothing if text is empty', () => {
-  const message = new Parsed18xxMessage({ text: '' });
+test("does nothing if text is empty", () => {
+  const message = new Parsed18xxMessage({ text: "" });
 
   expect(message).toEqual(emptyMessage);
 });
 
-test('does nothing if text does not match', () => {
-  const message = new Parsed18xxMessage({ text: 'asdasdasd' });
+test("does nothing if text does not match", () => {
+  const message = new Parsed18xxMessage({ text: "asdasdasd" });
 
   expect(message).toEqual(emptyMessage);
 });
 
-test('does nothing if input is empty', () => {
+test("does nothing if input is empty", () => {
   const message = new Parsed18xxMessage(JSON.parse('""'));
 
   expect(message).toEqual(emptyMessage);
 });
 
-test('does nothing if input has no property', () => {
-  const message = new Parsed18xxMessage(JSON.parse('{}'));
+test("does nothing if input has no property", () => {
+  const message = new Parsed18xxMessage(JSON.parse("{}"));
 
   expect(message).toEqual(emptyMessage);
 });
 
-test('parses message', () => {
+test("parses message", () => {
   const message = new Parsed18xxMessage(JSON.parse(event));
 
   expect(message).toEqual({
-    userId: '123456789',
-    text: 'Your Turn',
-    title: '1836Jr30',
-    description: 'Test game',
-    round: 'Auction Round',
+    userId: "123456789",
+    text: "Your Turn",
+    title: "1836Jr30",
+    description: "Test game",
+    round: "Auction Round",
     turn: 1,
-    link: 'http://18xx.games/game/1234',
+    link: "http://18xx.games/game/1234",
     valid: true,
   });
 });
 
-test('formats message text back to original', () => {
+test("parses 18ZOO message", () => {
+  const message = new Parsed18xxMessage({
+    text: '<@Hello> Your Turn in 18ZOO - Map A "test zoo" (SR-day 1)\nhttp://18xx.games/game/59585',
+  });
+
+  expect(message.valid).toBeTruthy();
+});
+
+test("formats message text back to original", () => {
   const message = new Parsed18xxMessage(JSON.parse(event));
 
   expect(message.toString()).toBe(
@@ -61,8 +69,8 @@ test('formats message text back to original', () => {
   );
 });
 
-test('formats empty message to nothing', () => {
-  const message = new Parsed18xxMessage({ text: '' });
+test("formats empty message to nothing", () => {
+  const message = new Parsed18xxMessage({ text: "" });
 
-  expect(message.toString()).toBe('');
+  expect(message.toString()).toBe("");
 });
